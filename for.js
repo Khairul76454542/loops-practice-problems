de91a0f073c676a4e329383a097b3fda79ec13d3
khairul76454542 <khairul76454542@gmail.com>
2023-08-02 02:01:42 +0600

    added same naw

--- for.js
+++ for.js
@@ -1,7 +1,21 @@
 var roastGiven = 0;
-while(roastGiven < 7){
-    console.log('roast den please !!')
-    roastGiven++;
+while (roastGiven < 7) {
+  console.log("roast den please !!");
+  roastGiven++;
 }
-
-for(var roastGiven = 0; roastGiven < 7 ; roa)
+// for loop
+for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+  console.log("roast den please !!");
+}
+// simple version of for loop
+console.log("simple for loop");
+for (var i = 0; i < 7; i++) {
+  console.log(i);
+}
+// i += 2;
+for (var i = 0; i <= 20; i += 2) {
+  console.log(i);
+}
+for (var i = 0; i <= 20; i += 2) {
+  console.log(i);
+}
